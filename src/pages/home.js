887636d65c74f097e9dfefbdfdb629df51312580
gibercode <giberlara@gmail.com>
2020-05-06 22:30:09 -0400

    scaffolding

---
+++ src/pages/home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { SEO } from '../components';
+import './styles/home.scss';
+const Home = () => {
+
+  return (
+    <div>
+       <SEO title='Home' />
+       <h1>Pix landing</h1>
+    </div>
+  )
+}
+
+export default Home
