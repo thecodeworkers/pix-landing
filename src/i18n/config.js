84f341c48d18f4c88dfdf316ce16770e148ff4a6
gibercode <giberlara@gmail.com>
2020-05-08 16:56:55 -0400

    multilanguage test

---
+++ src/i18n/config.js
@@ -0,0 +1,27 @@
+import i18next from 'i18next';
+
+i18next.init({
+    fallbackLng: 'es',
+    resources: {
+        es: {
+            translations: require('../assets/i18n/es.json')
+        },
+        en: {
+            translations: require('../assets/i18n/en.json')
+        }
+    },
+    ns: ['translations'],
+    defaultNS: 'translations',
+    returnObjects: true,
+    debug: process.env.NODE_ENV === 'development',
+    interpolation: {
+        escapeValue: false, // not needed for react!!
+    },
+    react: {
+        wait: true,
+    },
+});
+
+i18next.languages = ['es', 'en'];
+
+export default i18next;
