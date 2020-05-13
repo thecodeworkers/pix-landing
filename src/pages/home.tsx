aefb0a70c73c4515f1523bbf9b36c91ab29b870b
gibercode <giberlara@gmail.com>
2020-05-13 13:51:51 -0400

    Scrolldown and navbar changes

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -4,6 +4,7 @@ import { Navbar, Lang, Footer, Test, ScrollDown } from '../components';
 import { withTrans } from '../i18n/withTrans';
 import './styles/home.scss';
 import { useTranslation } from 'react-i18next';
+import axios from 'axios';
 
 const Home = (props) => {
   const { t, i18n } = useTranslation()
@@ -12,24 +13,11 @@ const Home = (props) => {
       const target = document.getElementById(id);
       window.scrollTo({ top: target.offsetTop - 100, behavior: 'smooth' });
   }
-
   return (
     <div>
       <SEO title='Home' />
       <Navbar />
-      <br></br><br></br><br></br>
-
-      <Lang />
-      <h1> {t('helloWorld')}</h1>
-
-      <Footer />
-      <ScrollDown scrollTo={scrolling} landing='target_div'/>
-
-      <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
-      <div id='target_div'>
-        <h1>hello</h1>
-      </div>
-
+    
     </div>
   )
 }
