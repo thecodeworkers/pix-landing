aa1bbdea6ecf348de31d5436e4f2bcd27e47fff3
gibercode <giberlara@gmail.com>
2020-05-13 00:01:43 -0400

    scroll down pixel

--- src/components/ScrollDown/index.tsx
+++ src/components/ScrollDown/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { PixelLogo } from '../Svg';
+import './style.scss';
 
 const ScrollDown = ({scrollTo, landing}) => {
-
    return (
      <div>
-       <button onClick={() => scrollTo(landing)}>scroll down</button>
+       <div className='_scrollDown' onClick={() => scrollTo(landing)}>  
+         <PixelLogo color='#FF8008' />
+       </div>
+       {/* <button >scroll down</button> */}
      </div>
    )
 }
--- src/components/Svg/index.ts
+++ src/components/Svg/index.ts
@@ -1,1 +1,7 @@
 export { default as PixelLogo } from './pixel-logo';
+export { default as Android } from './android-logo';
+export { default as Twitter } from './twitter-logo';
+export { default as Facebook } from './facebook-logo';
+export { default as Instagram } from './instagram-logo';
+export { default as Apple } from './apple-logo';
+export { default as PixLogo } from './pix-logo';
