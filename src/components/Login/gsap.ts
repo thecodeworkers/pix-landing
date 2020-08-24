3460f41204fe7bdb2c37564a442bc680ad73d5d6
gibercode <giberlara@gmail.com>
2020-08-24 19:22:36 -0400

    text animation

--- src/components/Login/gsap.ts
+++ src/components/Login/gsap.ts
@@ -1,235 +1,243 @@
-import {gsap , ScrollTrigger, TimelineMax} from 'gsap/all'
+import { gsap, ScrollTrigger, TimelineMax } from 'gsap/all'
 
 
-/* export const GSAP = () => {  */ 
+/* export const GSAP = () => {  */
 
 
 const gsapStart = () => {
-    gsap.registerPlugin(ScrollTrigger) 
-  
-   
-     gsap.to("._separatorLogin",{
+  gsap.registerPlugin(ScrollTrigger)
+
+
+  gsap.to("._separatorLogin", {
+    scrollTrigger: {
+      trigger: '._separatorLogin',
+    },
+    scale: 1,
+    opacity: 1,
+    duration: 0.6,
+    delay: 0.6
+  }),
+
+
+    gsap.to(["._loginBtnBlack", "._registerBtnBlue"], {
       scrollTrigger: {
-        trigger: '._separatorLogin',
+        trigger: "._loginBtnBlack",
       },
-      scale:1,
-     opacity: 1,  
-     duration: 2,
-     delay: 0.2
-   }),
- 
+      y: 0,
+      opacity: 1,
+      duration: 0.6,
+      delay: 0.8
+    })
 
-   gsap.to(["._loginBtnBlack","._registerBtnBlue"],{
+  gsap.to(['._footerScrollLogin', '._footerLangLogin'], {
     scrollTrigger: {
       trigger: "._loginBtnBlack",
     },
-    y:0,
-    opacity: 1,  
+    y: 0,
+    opacity: 1,
     duration: 0.6,
-    delay: 0.4
-  })
-
-  
-  gsap.to(['._footerScrollLogin', '._footerLangLogin'],{
-    scrollTrigger: {
-      trigger: "._loginBtnBlack",
-    },
-    y:0,
-    opacity: 1,  
-    duration: 0.6,
-    delay: 0.7
+    delay: 0.8
   }),
 
-  gsap.to("._boxLogin",{
-    scrollTrigger: {
-      trigger: '._boxLogin',
-    },
-    opacity:1,
-    y:0, 
-   duration: 2,
-   delay: 0.6
- }),
+    gsap.to("._boxLogin", {
+      scrollTrigger: {
+        trigger: '._boxLogin',
+      },
+      opacity: 1,
+      y: 0,
+      duration: 1,
+      delay: 0.8
+    }),
 
-  //PRODUCT
+    gsap.to(['._blackHeader', '._logoSmall', '._itemUno'], {
+      scrollTrigger: {
+        trigger: '._blackHeader',
+      },
+      y: 0,
+      duration: 0.6,
+      delay: 0.5
+    }),
 
-  gsap.to("._separatorProduct",{
-    scrollTrigger: {
-      trigger: '._separatorProduct',
-    },
-    duration: 1,
-    delay: 0.1,
-    width:'100%'
-  }),
+    //PRODUCT
 
-  gsap.to("._centerSquare",{
-    scrollTrigger: {
-      trigger: '._centerSquare',
-    },
-    y:0,
-    opacity: 1,
-    rotate:360,  
-    duration: 1.1,
-    delay: 0.3
-  }),
+    gsap.to("._separatorProduct", {
+      scrollTrigger: {
+        trigger: '._separatorProduct',
+      },
+      duration: 1,
+      delay: 0.1,
+      width: '100%'
+    }),
 
-  gsap.to("._productext",{
-    scrollTrigger: {
-      trigger: '._productext',
-    },
-    y:0,
-    opacity: 1,
-    duration: 1,
-    delay: 0.3
-  }),
+    gsap.to("._centerSquare", {
+      scrollTrigger: {
+        trigger: '._centerSquare',
+      },
+      y: 0,
+      opacity: 1,
+      rotate: 360,
+      duration: 1.1,
+      delay: 0.3
+    }),
 
-  gsap.to("._paragraphProduct",{
-    scrollTrigger: {
-      trigger: '._paragraphProduct',
-    },
-    y:0,
-    opacity: 1,
-    duration: 1,
-    delay: 0.4
-  }),
+    gsap.to("._productext", {
+      scrollTrigger: {
+        trigger: '._productext',
+      },
+      y: 0,
+      opacity: 1,
+      duration: 1,
+      delay: 0.3
+    }),
 
-  gsap.to("._cubeContainer",{
-    scrollTrigger: {
-      trigger: '._cubeContainer',
-    },
-    y:0,
-    opacity: 1,
-    duration: 1,
-    delay: 0.5
-  }),
+    gsap.to("._paragraphProduct", {
+      scrollTrigger: {
+        trigger: '._paragraphProduct',
+      },
+      y: 0,
+      opacity: 1,
+      duration: 1,
+      delay: 0.4
+    }),
 
-  ////INSTANT
+    gsap.to("._cubeContainer", {
+      scrollTrigger: {
+        trigger: '._cubeContainer',
+      },
+      y: 0,
+      opacity: 1,
+      duration: 1,
+      delay: 0.5
+    }),
 
-  gsap.to("._pixLeftDescriptionO",{
-    scrollTrigger: {
-      trigger: '._pixLeftDescriptionO',
-    },
-    y:0,
-    duration: 1,
-    delay: 0.2
-  }),
+    ////INSTANT
 
-  gsap.to("._separatorInstant",{
-    scrollTrigger: {
-      trigger: '._separatorInstant',
-    },
-    duration: 2,
-    delay: 0.2,
-    width:'100%'
-  }),
+    gsap.to("._pixLeftDescriptionO", {
+      scrollTrigger: {
+        trigger: '._pixLeftDescriptionO',
+      },
+      y: 0,
+      duration: 1,
+      delay: 0.2
+    }),
 
-  gsap.to("._orangePixContainer",{
-    scrollTrigger: {
-      trigger: '._orangePixContainer',
-    },
-    opacity:1,
-    duration: 1.5,
-    delay: 0.2
-  })
+    gsap.to("._separatorInstant", {
+      scrollTrigger: {
+        trigger: '._separatorInstant',
+      },
+      duration: 2,
+      delay: 0.2,
+      width: '100%'
+    }),
+
+    gsap.to("._orangePixContainer", {
+      scrollTrigger: {
+        trigger: '._orangePixContainer',
+      },
+      opacity: 1,
+      duration: 1.5,
+      delay: 0.2
+    })
 
   ////BannerImage
 
-  gsap.to("._separatorBanner",{
+  gsap.to("._separatorBanner", {
     scrollTrigger: {
       trigger: '._separatorBanner',
     },
-    scale:1,  
+    scale: 1,
     duration: 1,
     delay: 0.2
   }),
 
-  gsap.to("._imageContainerText",{
-    scrollTrigger: {
-      trigger: '._imageContainerText',
-    },
-    y:0,
-    duration: 0.1,
-    delay: 0.2
-  }),
-
-  
-
- /*  gsap.to(["._orange, ._coins"],{
-    scrollTrigger: {
-      trigger: '._imageContainerText',
-    },
-    y:0,
-    duration: 0.1,
-    delay: 0.2
-  }) */
-
-////ABOUT US
-  gsap.to("._aboutFather",{
-    scrollTrigger: {
-      trigger: '._aboutFather',
-    },
-    y:0,
-    opacity:1,
-    duration: 1,
-    delay: 0.2
-  }),
-
-//////BANNER
-
-  gsap.to("._childrenTitle",{
-    scrollTrigger: {
-      trigger: '._childrenTitle',
-    },
-    opacity:1,
-    duration: 1,
-    delay: 0.2
-  }),
-
-  gsap.to(['._bannerCards','._bannerButton', '._buttonText','._buttonGradient'  ],{
-    scrollTrigger: {
-      trigger: '._bannerCards',
-    },
-    y:0,
-    opacity:1,
-    duration: 1,
-    delay: 0.2
-  }),
+    gsap.to("._imageContainerText", {
+      scrollTrigger: {
+        trigger: '._imageContainerText',
+      },
+      y: 0,
+      duration: 0.1,
+      delay: 0.2
+    }),
 
 
 
-  ////STORE
+    /*  gsap.to(["._orange, ._coins"],{
+       scrollTrigger: {
+         trigger: '._imageContainerText',
+       },
+       y:0,
+       duration: 0.1,
+       delay: 0.2
+     }) */
 
-  gsap.to("._storeTitle",{
-    scrollTrigger: {
-      trigger: '._storeTitle',
-    },
-    y:0, 
-    opacity:1, 
-    duration: 1,
-    delay: 0.4
-  }),
+    ////ABOUT US
+    gsap.to("._aboutFather", {
+      scrollTrigger: {
+        trigger: '._aboutFather',
+      },
+      y: 0,
+      opacity: 1,
+      duration: 1,
+      delay: 0.2
+    }),
 
-  gsap.to("._storeLine",{
-    scrollTrigger: {
-      trigger: '._storeLine',
-    },
-    scale:1,  
-    duration: 1,
-    delay: 0.2
-  }),
+    //////BANNER
 
-  gsap.to(["._storeText","._storeButtonContainer", "._footerNetwork" ],{
-    scrollTrigger: {
-      trigger: '._storeText',
-    },
-    y:0, 
-    opacity:1, 
-    duration: 1,
-    delay: 0.5
-  })
+    gsap.to("._childrenTitle", {
+      scrollTrigger: {
+        trigger: '._childrenTitle',
+      },
+      opacity: 1,
+      duration: 1,
+      delay: 0.2
+    }),
+
+    gsap.to(['._bannerCards', '._bannerButton', '._buttonText', '._buttonGradient'], {
+      scrollTrigger: {
+        trigger: '._bannerCards',
+      },
+      y: 0,
+      opacity: 1,
+      duration: 1,
+      delay: 0.2
+    }),
+
+
+
+    ////STORE
+
+    gsap.to("._storeTitle", {
+      scrollTrigger: {
+        trigger: '._storeTitle',
+      },
+      y: 0,
+      opacity: 1,
+      duration: 1,
+      delay: 0.4
+    }),
+
+    gsap.to("._storeLine", {
+      scrollTrigger: {
+        trigger: '._storeLine',
+      },
+      scale: 1,
+      duration: 1,
+      delay: 0.2
+    }),
+
+    gsap.to(["._storeText", "._storeButtonContainer", "._footerNetwork"], {
+      scrollTrigger: {
+        trigger: '._storeText',
+      },
+      y: 0,
+      opacity: 1,
+      duration: 1,
+      delay: 0.5
+    })
 
   //////
- 
-  }
+
+}
 
 export default gsapStart
 
--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -1,74 +1,85 @@
 import React, { useEffect, useRef } from 'react';
 import { PixLogo } from '../Svg';
-import { CardCurrency, ScrollDown, Lang, Separator} from '../../components';
-import {BackgroundLogin} from './components'
+import { CardCurrency, ScrollDown, Lang, Separator } from '../../components';
+import { BackgroundLogin } from './components'
 import './style.scss';
- import gsapStart from './gsap'
+import gsapStart from './gsap'
 import { saveReference } from '../../store/actions';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { withTrans } from '../../i18n/withTrans';
 
-const Login = ({childrenTitle = null, scroll, action, about, product, t }) => {
+const Login = ({ childrenTitle = null, scroll, action, about, product, t }) => {
 
- 
+
   const { saveReference } = action;
-  const productRef = useRef();
+  // const productRef = useRef();
 
   gsapStart();
 
-  console.log(gsapStart,'LOGIN');
+  console.log(gsapStart, 'LOGIN');
+
+
+  /*    useEffect(() => {
+      if (product) saveReference({ productRef });
+       if (about) saveReference({ aboutRef });
+    }, [scroll.productRef]);
+    console.log(product , 'PRODUCT');
   
+   */
 
-/*    useEffect(() => {
-    if (product) saveReference({ productRef });
-     if (about) saveReference({ aboutRef });
-  }, [scroll.productRef]);
-  console.log(product , 'PRODUCT');
-
- */
-
- const benefits = {
-   fast: 'fast',
-   easy:'easy',
-   secure:'secure',
-   intuitive: 'intuitiva'
- }
+  const benefits = {
+    fast: 'fast',
+    easy: 'easy',
+    secure: 'secure',
+    intuitive: 'intuitiva'
+  }
 
   return (
 
     <div className='_imageLoginLight'>
-    
-        <div className='_logoSmall'>
-          <PixLogo color='#00021C' />
+
+      <div className='_logoSmall'>
+        <PixLogo color='#00021C' />
+      </div>
+      <div style={{ zIndex: 1, display: 'inline-flex' }}>
+
+        <div className='_parentTitlehey'>
+          <h4 className='_blackHeader'>{childrenTitle}
+          </h4>
+          <div className='_textSlider'>
+            <h4 className='_itemUno'>rápida</h4>
+            <h4 className='_itemDos'>fácil</h4>
+            <h4 className='_itemThree'>segura</h4>
+            <h4 className='_itemFour'>intuitiva</h4>
+          </div>
         </div>
-        <div style={{zIndex: 1}}>
-        <h4 className='_blackHeader'>{childrenTitle} Rapida</h4>
-        </div>
-        <div className='_separatorLogin'>
-          <Separator height={10} />
-        </div>
-        <div className='_buttonContainerLogin'>
+
+      </div>
+      <div className='_separatorLogin'>
+        <Separator height={10} />
+      </div>
+      <div className='_buttonContainerLogin'>
         <button className='_loginBtnBlack'>{t('login').toUpperCase()}</button>
         <button className='_registerBtnBlue'>{t('register').toUpperCase()}</button>
-       
-        </div>
-        
-        <div className="_footerScrollLogin">
-            <div className="_scrollDimensionLogin">
-              <ScrollDown color="#fff" reference={scroll.productRef}/>
-            </div>
-          </div>
 
-        <div className="_footerLangLogin">
-            <Lang/>
+      </div>
+
+      <div className="_footerScrollLogin">
+        <div className="_scrollDimensionLogin">
+          <ScrollDown color="#fff" reference={scroll.productRef} />
         </div>
-       
-       
+      </div>
+
+      <div className="_footerLangLogin">
+        <Lang />
+      </div>
+
+
 
       <div className='_cubito'>
         <BackgroundLogin />
-        </div>
+      </div>
     </div>
   )
 };
