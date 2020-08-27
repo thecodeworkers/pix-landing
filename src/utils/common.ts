547b012c9a78f54e4bc936356aee19a6637acd72
gibercode <giberlara@gmail.com>
2020-08-27 04:25:08 -0400

    general style changes

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,6 +1,8 @@
 export const actionObject = (type: string, payload: any = null) => ({ type, payload });
 
 export const scrolling = (ref: any): void => {
-  const target = ref.current;
-  window.scrollTo({ top: target.offsetTop, behavior: 'smooth' });
+  if(ref) {
+    const target = ref.current;
+    window.scrollTo({ top: target.offsetTop, behavior: 'smooth' });
+  } 
 }
