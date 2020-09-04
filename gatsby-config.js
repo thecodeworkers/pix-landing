20420cd48dd8b176197f93dadd5a3017229b9973
gibercode <giberlara@gmail.com>
2020-09-04 11:24:45 -0400

    favicon added

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,7 +26,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/pix-ico.png`, // This path is relative to the root of the site.
+        icon: `src/assets/img/favicon.ico`, // This path is relative to the root of the site.
       },
       resolve: 'gatsby-plugin-google-fonts',
       options: {
