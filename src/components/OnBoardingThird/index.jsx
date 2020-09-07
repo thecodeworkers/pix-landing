f819cf008035aebfb1fffbbb4a1bfdf4ba8c9ec8
gibercode <giberlara@gmail.com>
2020-09-07 16:54:51 -0400

    onboarding third screen changes

--- src/components/OnBoardingThird/index.jsx
+++ src/components/OnBoardingThird/index.jsx
@@ -1,58 +1,22 @@
-import React, { useEffect, createRef, useState } from 'react';
+import React, { useEffect } from 'react';
 import './styles.scss';
 import { PhoneLight, BtcExchangeEth, PhoneDark, BtcExchangeUsd, SendBtcCard } from '../Svg';
 import { bindActionCreators } from 'redux';
 import { saveStep } from '../../store/actions';
 import { connect } from 'react-redux';
 import { withTrans } from '../../i18n/withTrans';
-import lottie from 'lottie-web';
-import animation1 from '../../assets/animations/exchangeLeft.json';
 import { TimelineMax } from 'gsap/all';
 
 const OnBoardingThird = (props) => {
 
   const { onboarding, action , t } = props;
-  const [animation, setAnimation ] = useState(null);
-  // const [animationTwo, setAnimationTwo ] = useState(null);
-
-
   const timeLine = new TimelineMax({ paused: true });
-  // const animationContainer = createRef()
-  // const animationContainerTwo = createRef()
-
-  // let anim
-
-//   useEffect(() => {
-//     const anim = lottie.loadAnimation({
-//     container: animationContainer.current,
-//     renderer: "svg",
-//     loop: true,
-//     autoplay: false,
-//     animationData: animation1
-//   });
-
-//   const anim2 = lottie.loadAnimation({
-//     container: animationContainerTwo.current,
-//     renderer: "svg",
-//     loop: true,
-//     autoplay: false,
-//     animationData: animation1
-//   });
-
-//   setAnimation(anim);
-//   setAnimationTwo(anim2);
-
-// //  anim.addEventListener('complete', () => showContent());
-//   return () => anim.destroy();
-// }, []);
 
   useEffect(() => {
     if(onboarding.stepTwo) playanimation();
   }, [onboarding.stepTwo]);
 
   const playanimation = () => {
-    console.log(animation);
-
     timeLine.play()
       .to('._descriptionSectionParragraph', 0.6, { y: 0, opacity: 1 }, 0.6)
       .to('._phoneLigthDeviceParent', 0.6, { x: 0 }, 0.4)
@@ -61,9 +25,6 @@ const OnBoardingThird = (props) => {
       .to('._descriptionSectionRightParragraph', 0.6, { y: 0, opacity: 1 }, 1)
       .to('._senCard', 0.6, { bottom: '5%', opacity: 1 }, 0.9)
       timeLine.eventCallback("onComplete", () => showLanding());
-
-      // animation.play();
-      // animationTwo.play();
   }
 
   const showLanding = () => {
